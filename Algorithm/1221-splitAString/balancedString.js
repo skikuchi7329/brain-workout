b3e4d75cd4e3171c9cb919eb9564c537cf19b283
Shohei Kikuchi <killchance2256@gmail.com>
2020-10-10 20:47:12 +0900

    balanced

---
+++ Algorithm/1221-splitAString/balancedString.js
@@ -0,0 +1,17 @@
+const balancedStringSplit = function(s) {
+  const result = [];
+  // const countR = 0;
+  // const countL = 0;
+  // const exR = /R/g;
+  // const exL = /L/g;
+
+  // const arrR = s.match(exR);
+  // const arrL = s.match(exL);
+  // console.log(arrL)
+  for (let i = 0; i < s.length; i++)
+    if(str[i] = )
+
+}
+
+const s = "RLRRLLRLRL";
+console.log(balancedStringSplit(s));
