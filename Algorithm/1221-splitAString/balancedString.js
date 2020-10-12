78dca46139b30581c69c60d8b3fff4e3f780f932
Shohei Kikuchi <killchance2256@gmail.com>
2020-10-12 17:51:33 +0900

    1221 solving

--- Algorithm/1221-splitAString/balancedString.js
+++ Algorithm/1221-splitAString/balancedString.js
@@ -1,17 +1,20 @@
 const balancedStringSplit = function(s) {
-  const result = [];
-  // const countR = 0;
-  // const countL = 0;
-  // const exR = /R/g;
-  // const exL = /L/g;
-
-  // const arrR = s.match(exR);
-  // const arrL = s.match(exL);
-  // console.log(arrL)
-  for (let i = 0; i < s.length; i++)
-    if(str[i] = )
-
+  
+  for (let i = 0; i < s.length; i++) {
+    const result = 0;
+    const countR = 0;
+    const countL = 0;
+    if (s.charAt(i) === "R") {
+      countR++;
+    } else if (s.charAt(i) === "L") {
+      countL++;
+    } else if (countR === countL) {
+      result++;
+    }
+  }
+  return result;
 }
+
 
 const s = "RLRRLLRLRL";
 console.log(balancedStringSplit(s));
