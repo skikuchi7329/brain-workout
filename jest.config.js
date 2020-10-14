8e0499af20f726b6caca333b98b2a3fa79c8380b
Shohei Kikuchi <killchance2256@gmail.com>
2020-10-14 18:08:11 +0900

    add test files

--- jest.config.js
+++ jest.config.js
@@ -8,3 +8,11 @@ module.exports = {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
 };
+
+module.exports = {
+  // OTHER PORTIONS AS MENTIONED BEFORE
+
+  // Setup Enzyme
+  "snapshotSerializers": ["enzyme-to-json/serializer"],
+  "setupFilesAfterEnv": ["<rootDir>/src/setupEnzyme.ts"],
+}
